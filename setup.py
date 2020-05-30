0ebf44be7bc3c902704a1e0e6878a20768c20907
Chris McCormick <chris@mccormick.cx>
2020-05-30 17:10:21 +0800

    Distribution on pypi.

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
+#!/usr/bin/env python
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
-    name="Un-template",
+    name="ntpl",
     version="0.0.1",
     author="Chris McCormick",
     author_email="chris@mccormick.cx",
     packages=["ntpl"],
     license="LICENSE.txt",
-    long_description=open("README.md").read(),
-    long_description_content_type='text/markdown',
+    description="Minimal library to modify and render HTML.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/chr15m/ntpl",
     install_requires=[
         "beautifulsoup4==4.9.1",
         "pyhiccup==0.1",
