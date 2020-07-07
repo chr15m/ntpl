d798fd826e882d77755cd8ddf1c5f75cebc4aee0
Chris McCormick <chris@mccormick.cx>
2020-07-07 14:49:47 +0800

    Bump verison.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="ntpl",
-    version="0.0.1",
+    version="0.0.2",
     author="Chris McCormick",
     author_email="chris@mccormick.cx",
     packages=["ntpl"],
